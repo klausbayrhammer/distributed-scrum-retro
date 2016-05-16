e830ee88569f2da1a7e32f5f858d5f7f7acf0501
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2016-05-16 19:50:55 +0200

    removes bodyparser (use queryparams for now)

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const apiRoute = require('./api');
 const websockets = require('./websockets');
-const bodyParser = require('body-parser');
 
 const app = express();
-
-app.use(bodyParser.urlencoded());
-
-app.use(bodyParser.json());
 
 app.use('/api', apiRoute());
 
